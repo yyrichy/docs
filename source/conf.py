d11586d5379f80dc8e1b88eac00b2ac48574689d
VapoR <ryvapor@gmail.com>
2022-02-03 10:17:28 -0500

    add spihnx-panels

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.autosectionlabel'
+    'sphinx.ext.autosectionlabel',
+    'sphinx_panels'
 ]
 
 intersphinx_mapping = {
